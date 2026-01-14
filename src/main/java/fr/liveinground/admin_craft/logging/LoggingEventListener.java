6b534b41e5b38ca62bc345827e2d4a41a48ca944
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2026-01-14 19:35:42 +0100

    Update LoggingEventListener.java

--- src/main/java/fr/liveinground/admin_craft/logging/LoggingEventListener.java
+++ src/main/java/fr/liveinground/admin_craft/logging/LoggingEventListener.java
@@ -1,4 +1,13 @@
 package fr.liveinground.admin_craft.logging;
 
 public class LoggingEventListener {
+
+    @SubscribeEvent(priority=EventPriority.LOWEST)
+    public void onExplode(ExplosionEvent event) {
+        if (!event.isCancelled) {
+            for (BlockPos pos : event.getExplosion().getToBlow()) {
+                // todo: regiser log entry for #explosion
+            }
+        }
+    }
 }
