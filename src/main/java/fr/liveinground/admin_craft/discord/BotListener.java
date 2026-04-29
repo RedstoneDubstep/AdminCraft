14d97690c513ccc15899bab0da972dc0f2ee8b8b
LiveinGround <129398571+LiveInGround@users.noreply.github.com>
2026-04-29 21:57:19 +0200

    Starting implementing discord bot for the appeal system

---
+++ src/main/java/fr/liveinground/admin_craft/discord/BotListener.java
@@ -0,0 +1,109 @@
+package fr.liveinground.admin_craft.discord;
+
+import fr.liveinground.admin_craft.storage.types.sanction.SanctionData;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
+
+import java.awt.*;
+
+public class BotListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getMessage().getContentRaw().equals("!AC ping")) {
+            event.getMessage().reply("AdminCraft loaded and appeal system ready.").queue();
+        }
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("status")) {
+
+            String target = event.getOption("target").getAsString();
+            if (target.equals("appeals")) {
+                if (DiscordBot.enabled) {
+                    event.reply("Appeal system is enabled.").queue();
+                } else {
+                    event.reply("Appeal system is disabled").queue();
+                }
+            } else if (target.equals("server")) {
+                event.reply("Server is online.").queue();
+            } else {
+                event.reply("'target' argument has to be either 'appeals' or 'server'.").setEphemeral(true).queue();
+            }
+        }
+        else if (event.getName().equals("post_embed")) {
+
+            String title = event.getOption("title").getAsString();
+            String description = event.getOption("description").getAsString();
+            String emojiName = event.getOption("emoji").getAsString();
+
+            Emoji emoji = Emoji.fromFormatted(emojiName);
+
+            String label = event.getOption("label").getAsString();
+
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.setTitle(title);
+            builder.setDescription(description);
+            builder.setColor(Color.CYAN);
+
+            event.getChannel().sendMessageEmbeds(builder.build()).setActionRow(Button.secondary(DiscordBot.INFO_BUTTON_ID, label)
+                    .withEmoji(emoji)).queue();
+        }
+    }
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+        if (event.getButton().getId().equals(DiscordBot.INFO_BUTTON_ID)) {
+            Modal modal = Modal.create(DiscordBot.INFO_MODAL_ID, "Sanction data")
+                    .addActionRow(TextInput.create("id", "Sanction ID", TextInputStyle.SHORT).setPlaceholder("e.g. ").build()) //todo: placeholder and min-max
+                    .addActionRow(TextInput.create("ign", "Minecraft username", TextInputStyle.SHORT).setPlaceholder("e.g. Steve").setMinLength(3).setMaxLength(16).build())
+                    .build();
+            event.replyModal(modal).queue();
+        } else if (event.getButton().getId().equals(DiscordBot.APPEAL_BUTTON_ID)) {
+            Modal modal = Modal.create(DiscordBot.APPEAL_MODAL_ID, "Appeal form")
+                    .addActionRow(TextInput.create("reason", "Appeal reason", TextInputStyle.PARAGRAPH).setPlaceholder("Why do you want to appeal your sanction ?").build())
+                    .build();
+            event.replyModal(modal).queue();
+        }
+    }
+
+    @Override
+    public void onModalInteraction(ModalInteractionEvent event) {
+        if (event.getId().equals(DiscordBot.INFO_BUTTON_ID)) {
+            String id = event.getValue("id").getAsString();
+            String ign = event.getValue("ign").getAsString();
+            //todo: check id with ign, get data
+            SanctionData data;
+            String uuid;
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("Sanction information");
+            embed.setDescription("Please review your sanction's details before appealing.\n:warning: **WARNING:** Any abuse may be punished!");
+            embed.setColor(Color.RED);
+
+            embed.addField("IGN", ign, true);
+            embed.addField("UUID", uuid, true);
+            embed.addField("Sanction ID", id, true);
+            embed.addField("Sanction type", data.sanctionType, true);
+            embed.addField("Date", data.date, true);
+            if (data.expiresOn != null) {
+                embed.addField("Expires on", data.expiresOn, true);
+            }
+
+            event.replyEmbeds(embed.build()).addActionRow(Button.success(DiscordBot.APPEAL_BUTTON_ID, "Appeal")).queue();
+
+        } else if (event.getId().equals(DiscordBot.APPEAL_MODAL_ID)) {
+
+        }
+    }
+}
---
+++ src/main/java/fr/liveinground/admin_craft/discord/DiscordBot.java
@@ -0,0 +1,62 @@
+package fr.liveinground.admin_craft.discord;
+
+import fr.liveinground.admin_craft.AdminCraft;
+import fr.liveinground.admin_craft.Config;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiscordBot {
+    public static JDA jda;
+    public static boolean enabled;
+    public static Guild guild;
+
+    public static final String INFO_BUTTON_ID = "info_button";
+    public static final String APPEAL_BUTTON_ID = "appeal_button";
+    public static final String INFO_MODAL_ID = "info_modal";
+    public static final String APPEAL_MODAL_ID = "appeal_modal";
+
+    public static Map<String, String> data = new HashMap<>();
+
+    public static void start() {
+        AdminCraft.LOGGER.info("Starting appeal bot...");
+        if (Config.enable_appeals && !Config.bot_token.equals("configthisplease")) {
+            JDABuilder builder = JDABuilder.createDefault(Config.bot_token);
+            builder.addEventListeners(new BotListener());
+            jda = builder.build();
+            register_commands();
+            try {
+                jda.awaitReady();
+                AdminCraft.LOGGER.info("Appeal system enabled!");
+                enabled = true;
+            } catch (InterruptedException e) {
+                AdminCraft.LOGGER.error("Error while starting appeal bot: {}", String.valueOf(e));
+                enabled = false;
+            }
+
+        } else {
+            if (!Config.enable_appeals) {
+                AdminCraft.LOGGER.info("Appeal system disabled, skipping...");
+            } else {
+                AdminCraft.LOGGER.warn("Bot token was not configured, skipping...");
+            }
+            enabled = false;
+        }
+    }
+
+    private static void register_commands() {
+        guild.updateCommands().addCommands(Commands.slash("status", "Get bot or server status")
+                .addOption(OptionType.STRING, "target", "What do you want to get the status of?", true)).queue();
+        guild.updateCommands().addCommands(Commands.slash("post_embed", "Makes the bot send the appeal embed")
+                .addOption(OptionType.STRING, "title", "The embed title", true)
+                .addOption(OptionType.STRING, "description", "The embed description", true)
+                .addOption(OptionType.STRING, "emoji", "The button emoji", true)
+                .addOption(OptionType.STRING, "label", "The button label", true)).queue();
+    }
+}
