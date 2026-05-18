fd100d56aa5b90bfbae1cb99bfc303c00fdcaee8
LiveinGround <129398571+LiveInGround@users.noreply.github.com>
2026-05-18 19:09:28 +0200

    Finishing implementing the /ban command override

--- src/main/java/fr/liveinground/admin_craft/commands/moderation/BanCommand.java
+++ src/main/java/fr/liveinground/admin_craft/commands/moderation/BanCommand.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import fr.liveinground.admin_craft.AdminCraft;
 import fr.liveinground.admin_craft.Config;
 import fr.liveinground.admin_craft.moderation.CustomSanctionSystem;
+import fr.liveinground.admin_craft.moderation.SanctionConfig;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -15,6 +16,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.server.players.NameAndId;
 
 import java.util.Collection;
+import java.util.Date;
 
 public class BanCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher, boolean override) {
@@ -22,7 +24,7 @@ public class BanCommand {
         if (override) {
             commandName = "ban";
         } else {
-            commandName = "acban";
+            commandName = "admincraft:ban";
         }
         dispatcher.register(Commands.literal(commandName)
                 .requires(source -> source.hasPermission(Config.ban_level))
@@ -31,9 +33,19 @@ public class BanCommand {
                             ban(ctx, "Banned by an operator");
                             return 1;
                         })
-                        .then(Commands.argument("reason", StringArgumentType.greedyString())
+                        .then(Commands.argument("args", StringArgumentType.greedyString())
+                                .suggests((ctx, builder) -> {
+                                    String remaining = builder.getRemaining();
+                                    if (!remaining.contains("--noappeal")) {
+                                        builder.suggest("--noappeal");
+                                    }
+                                    if (!remaining.contains("--delayedappeal:")) {
+                                        builder.suggest("--delayedappeal:");
+                                    }
+                                    return builder.buildFuture();
+                                })
                                 .executes(ctx -> {
-                                    ban(ctx, StringArgumentType.getString(ctx, "reason"));
+                                    ban(ctx, StringArgumentType.getString(ctx, "args"));
                                     return 1;
                                 })
                         )
@@ -41,14 +53,45 @@ public class BanCommand {
         );
     }
 
-    private static void ban(CommandContext<CommandSourceStack> ctx, String reason) throws CommandSyntaxException {
+    private static void ban(CommandContext<CommandSourceStack> ctx, String args) throws CommandSyntaxException {
+        String reason;
+        boolean appealable = Config.default_can_appeal;
+        Date appealDelay = SanctionConfig.getDurationAsDate(Config.default_appeal_delay);
+        String[] splitted = args.split(" ");
+        StringBuilder builder = new StringBuilder();
+
+        for (String i: splitted) {
+            if (i.equals("--noappeal")) appealable = false;
+            else if (i.contains("--delayedappeal:")) {
+                String[] delaysplit = i.split(":");
+                if (delaysplit.length == 2) {
+                    appealDelay = SanctionConfig.getDurationAsDate(delaysplit[1]);
+                    if (appealDelay == null) {
+                        ctx.getSource().sendFailure(Component.literal("Invalid duration format for the appeal delay.").withStyle(ChatFormatting.RED));
+                        return;
+                    }
+                }
+            } else builder.append(" ").append(i);
+        }
+        if (!builder.isEmpty()) {
+            builder.delete(0, 0);
+            reason = builder.toString();
+        }
+        else {
+            reason = "Banned by an operator";
+        }
+
         Collection<NameAndId> collection = GameProfileArgument.getGameProfiles(ctx, "player");
         NameAndId target = AdminCraft.getOneProfile(collection);
         if (target == null) {
             ctx.getSource().sendFailure(Component.literal("No player was found").withStyle(ChatFormatting.RED));
             return;
         }
-        CustomSanctionSystem.banPlayer(ctx.getSource().getServer(), ctx.getSource().getTextName(), target, reason, null, false, null);
+        if (ctx.getSource().getServer().getPlayerList().getBans().isBanned(target)) {
+            ctx.getSource().sendFailure(Component.literal("This player is already banned.").withStyle(ChatFormatting.RED));
+            return;
+        }
+        CustomSanctionSystem.banPlayer(ctx.getSource().getServer(), ctx.getSource().getTextName(), target, reason, null, appealable, appealDelay);
         ctx.getSource().sendSuccess(() -> Component.literal("Banned " + target.name() + ": " + reason), true);
     }
 }
